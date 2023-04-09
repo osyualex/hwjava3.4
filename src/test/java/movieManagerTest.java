ecab9fda6fa9a4561180693d2d0f82123b08885d
Aleksandr Osyukov <ocukov@mail.ru>
2023-04-09 12:00:21 +0300

    add manager

---
+++ src/test/java/movieManagerTest.java
@@ -0,0 +1,43 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+public class movieManagerTest {
+
+    @Test
+    public void testMovieManagerAdd(){
+        movieManager manager  = new movieManager();
+
+        manager.add("Film 1");
+
+
+        String[] expected = {"Film 1"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testMovieManagerAll(){
+        movieManager manager  = new movieManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+        String[] expected = {"Film 1","Film 2","Film 3"};
+        String[] actual = manager.findAll();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testMovieManagerLast(){
+        movieManager manager  = new movieManager();
+
+        manager.add("Film 1");
+        manager.add("Film 2");
+        manager.add("Film 3");
+
+        String[] expected = {"Film 3","Film 2","Film 1"};
+        String[] actual = manager.findLast();
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+
+}
